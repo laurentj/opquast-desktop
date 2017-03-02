23d621dbb00ba1ff924bc8ee8d5cd7f29a8dc4c6
Laurent Jouanneau <dev@ljouanneau.com>
2017-03-02 15:39:55 +0100

    [webext] migrate the toolbar button

--- webextension/main.js
+++ webextension/main.js
@@ -17,3 +17,24 @@ console.log("Message from legacy add-on: " + message.content);
 });
 
 */
+
+
+let panelCmd = function(aRunTest, aCanClose) {
+    aRunTest = typeof(aRunTest) === "undefined" ? false : aRunTest;
+    aCanClose = typeof(aCanClose) === "undefined" ? true : aCanClose;
+
+    browser.runtime.sendMessage({
+        command: "legacy-panel-command",
+        runTest: aRunTest,
+        canClose: aCanClose
+    }).then(reply => {
+    });
+};
+
+
+browser.browserAction.onClicked.addListener(panelCmd.bind(null, false));
+
+
+
+
+
