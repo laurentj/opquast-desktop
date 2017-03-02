e965f7b38a7c2504c739b3c62d9fe5ffd25a55f0
Laurent Jouanneau <dev@ljouanneau.com>
2017-03-02 14:41:12 +0100

    [webext] skeleton of the web extension
    We embed a web extension, that is based on the new extension
    system of firefox.
    
    Features of the addon will be migrated one after an other
    to this embedded web extension.

---
+++ webextension/main.js
@@ -0,0 +1,19 @@
+// main background script of the extension
+
+/*
+// connection less messaging
+browser.runtime.sendMessage("message-from-webextension").then(reply => {
+    if (reply) {
+        console.log("response from legacy add-on: " + reply.content);
+    }
+});
+
+
+//Connection-oriented messaging
+var port = browser.runtime.connect({name: "connection-to-legacy"});
+
+port.onMessage.addListener(function(message) {
+console.log("Message from legacy add-on: " + message.content);
+});
+
+*/
